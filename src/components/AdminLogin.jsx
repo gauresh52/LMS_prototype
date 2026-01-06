cce8cd6140bc767895a094b048010a4e1be202ee
Guaresh Navlo Rekdo <guaresh@Guareshs-MacBook-Air.local>
2026-01-06 12:06:06 +0530

    ui optimization on admin login

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
 
 const ADMIN_USERNAME = "Admin";
 const ADMIN_PASSWORD = "Admin@567";
@@ -19,8 +21,10 @@ export default function AdminLogin({ onSuccess, onCancel }) {
     }
   };
 
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <div className="bg-white p-6 rounded shadow max-w-md mx-auto fade-in">
+    <div className="bg-white mt-6 p-8 rounded shadow max-w-md mx-auto fade-in f ">
       <h2 className="text-xl font-semibold mb-4">Admin Login</h2>
 
       <form onSubmit={handleLogin} className="space-y-4">
@@ -36,21 +40,32 @@ export default function AdminLogin({ onSuccess, onCancel }) {
         </div>
 
         <div>
-          <label className="block text-sm font-medium mb-1">Password</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full border rounded px-3 py-2"
-            placeholder="Enter password"
-          />
+          <label className="label mb-1">Password</label>
+
+          <div className="relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="input pr-10"
+              placeholder="Enter password"
+            />
+
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute right-2 top-1/2 -translate-y-1/2 px-1 text-gray-600"
+            >
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </button>
+          </div>
         </div>
 
-        {error && (
-          <p className="text-red-600 text-sm">{error}</p>
-        )}
+        {error && <p className="text-red-600 text-sm">{error}</p>}
 
-        <h6 className="text-x font-semibold mb-4">Admin seeded as Username:"Admin" & Pass:"Admin@567".</h6>
+        <h6 className="text-x font-semibold mb-4">
+          Admin seeded as Username:"Admin" & Pass:"Admin@567".
+        </h6>
 
         <div className="flex justify-end gap-2">
           <button
@@ -67,11 +82,8 @@ export default function AdminLogin({ onSuccess, onCancel }) {
           >
             Login
           </button>
-
         </div>
       </form>
-       
     </div>
-    
   );
 }
